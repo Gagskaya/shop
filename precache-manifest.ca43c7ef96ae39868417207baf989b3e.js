138049cf6907108abec651b6031b6c19748eaca6
Gagskaya <alisxan006@mail.ru>
2021-04-27 13:44:05 +0300

    Updates

---
+++ precache-manifest.ca43c7ef96ae39868417207baf989b3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26c0bb6857c77c0525b7d0c169906845",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "72bce3c86263a95acb42",
+    "url": "/shop/static/css/main.c3dc75b5.chunk.css"
+  },
+  {
+    "revision": "056055c825405e2d911a",
+    "url": "/shop/static/js/2.9f73bfa8.chunk.js"
+  },
+  {
+    "revision": "2089965cc3e0fbb1dda2d650d8277df4",
+    "url": "/shop/static/js/2.9f73bfa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72bce3c86263a95acb42",
+    "url": "/shop/static/js/main.c0e73d12.chunk.js"
+  },
+  {
+    "revision": "a6c552b2b733dce4bdda",
+    "url": "/shop/static/js/runtime-main.b119945a.js"
+  }
+]);
